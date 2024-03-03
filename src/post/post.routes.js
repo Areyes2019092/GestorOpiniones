59c097cb1907c756884ed4a4a682a26f0ad4be99
Angel <areyes-2019092@kinal.edu.gt>
2024-03-03 14:42:38 -0600

    Termine routes de post

--- src/post/post.routes.js
+++ src/post/post.routes.js
@@ -6,3 +6,37 @@ import { postDelete, postGet, postPost, postPut } from "./post.controller";
 
 const router = Router();
 
+router.get("/", postGet);
+
+router.delete(
+    "/:id",
+    [
+        validarJWT,
+        encontrarPost,
+        validarInformacion
+    ], postDelete
+);
+
+router.post(
+    "/",
+    [
+        validarJWT,
+        check("principal","La informacion es obligatoria").not().isEmpty(),
+        check("categoria","La categoria es obligatoria").not().isEmpty(),
+        check("contenido","El contenido es obligatorio").isLength({
+            min: 1
+        }),
+        validarInformacion
+    ],postPost
+);
+
+router.put(
+    "/:id",
+    [
+        validarJWT,
+        encontrarPost,
+        validarInformacion
+    ],postPut
+);
+
+export default router;
