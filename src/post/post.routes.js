94e5d879ea7e4fa002d482f273ea7c63f9c8fd69
Angel <areyes-2019092@kinal.edu.gt>
2024-03-03 14:25:44 -0600

    Empezando routes

--- src/post/post.routes.js
+++ src/post/post.routes.js
@@ -1,2 +1,8 @@
 import { check } from "express-validator";
 import { Router } from "express";
+import { validarJWT } from "../middlewares/validar-jwt";
+import { encontrarPost ,validarInformacion } from "../middlewares/validarCampos";
+import { postDelete, postGet, postPost, postPut } from "./post.controller";
+
+const router = Router();
+
