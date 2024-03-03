43661602bb5dfb6faed893dbdb3ca83a6bae900f
Angel <areyes-2019092@kinal.edu.gt>
2024-03-03 14:54:52 -0600

    Termine routes de las respuestas

--- src/respuestas/respuesta.routes.js
+++ src/respuestas/respuesta.routes.js
@@ -1,2 +1,41 @@
 import { check } from "express-validator";
 import { Router } from "express";
+import { validarInformacion, encontrarRespuesta,  idRepetido} from "../middlewares/validarCampos";
+import { validarJWT } from "../middlewares/validar-jwt";
+import {  respuestaGet , respuestaPut ,respuestaPost ,respuestaDelete } from "./respuesta.controller";
+
+const router = Router();
+
+router.get("/",respuestaGet);
+
+router.put(
+    "/:id",
+    [
+        validarJWT,
+        encontrarRespuesta,
+        validarInformacion
+    ],respuestaPut
+    );
+
+
+router.delete(
+    "/:id",
+    [
+        validarJWT,
+        encontrarRespuesta,
+        validarInformacion
+    ],respuestaDelete
+);
+
+
+router.post(
+    "/:id",
+    [
+        validarJWT,
+        idRepetido,
+        check("contenido","El texto no puede ir vacio").isLength({
+            min: 1
+        }),
+        validarInformacion
+    ],respuestaPost
+);
